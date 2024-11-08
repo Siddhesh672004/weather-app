13e1c28f7341d97d2e976e6a1241124ef01c110a
Siddhesh Chaudhari <siddheshavic67@gmail.com>
2024-11-08 13:21:41 +0530

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import Weather from './components/Weather.jsx'
+
+createRoot(document.getElementById('root')).render(
+    <>
+    <App />
+    </>
+)
